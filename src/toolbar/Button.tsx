3c05293ef53f07434383f4a875e3c149bda6e2cb
James H <james.s.hart@outlook.com>
2019-08-10 23:19:47 +0100

    made onClick optional

--- src/toolbar/Button.tsx
+++ src/toolbar/Button.tsx
@@ -6,7 +6,7 @@ import styles from './r_toolbar.css';
 
 import { Icon } from 'office-ui-fabric-react/lib/Icon';
 
-const ToolbarButton: React.FunctionComponent<{ icon?: string, children: string, onClick: React.MouseEventHandler }> = (props) => {
+const ToolbarButton: React.FunctionComponent<{ icon?: string, children: string, onClick?: React.MouseEventHandler }> = (props) => {
 	return (
 		<button onClick={props.onClick} className={styles.button}><Icon className={menuStyles.menu_button_icon} iconName={props.icon} /><label className={menuStyles.menu_button_label}>{props.children}</label></button>
 	)
