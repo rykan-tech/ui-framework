ef873e4a3344100c214d54227a9332d183c3f6db
James H <james.s.hart@outlook.com>
2019-08-13 09:08:50 +0100

    toolbar tweak

--- src/toolbar/index.tsx
+++ src/toolbar/index.tsx
@@ -6,8 +6,8 @@ import { Depths } from '@uifabric/fluent-theme';
 
 const Toolbar: React.FunctionComponent<{ children: any, style?: React.CSSProperties }> = (props) => {
 	return (
-		<div className={styles.toolbar_container}>
-			<div className={styles.toolbar} style={{ boxShadow: Depths.depth16, ...props.style }}>{props.children}</div>
+		<div className={styles.toolbar_container} style={{ boxShadow: Depths.depth16 }}>
+			<div className={styles.toolbar} style={{ ...props.style }}>{props.children}</div>
 		</div>
 	)
 }
