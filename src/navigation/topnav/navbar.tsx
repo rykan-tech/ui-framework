54ee79c41a1ad510831a1670db029d493acf4f4e
James Hart <james.s.hart@outlook.com>
2019-09-01 15:26:17 +0100

    added addition styles

--- src/navigation/topnav/navbar.tsx
+++ src/navigation/topnav/navbar.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 
 import styles from './rwf.css';
 
+import ThemeContext from '../../ThemeContext';
+
 export default class Navbar extends React.Component<{logo?: string, style?: React.CSSProperties, outerStyle?: React.CSSProperties, fixed?: boolean}> {
 	render() {
 		const {
@@ -9,16 +11,21 @@ export default class Navbar extends React.Component<{logo?: string, style?: Reac
 			logo,
 			style,
 			outerStyle,
-			//fixed,
 		} = this.props;
 
 		return (
-			<div className={styles.navbar} style={outerStyle}>
-				<div className={styles["navbar-inner"]} style={style}>
-					<img className={styles.rykan} src={logo}/>
-					{children}
-				</div>
-			</div>
+			<ThemeContext.Consumer>
+				{(theme) => {
+					return (
+						<div className={styles.navbar} style={outerStyle}>
+							<div className={styles["navbar-inner"]} style={{ background: theme.altHigh, ...style }}>
+								<img className={styles.rykan} src={logo} />
+								{children}
+							</div>
+						</div>
+					)
+				}}
+			</ThemeContext.Consumer>
 		)
 	}
 }
