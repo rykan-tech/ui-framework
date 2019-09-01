78fe965dab564c3bb41025b3ae05c773e81b88d5
James Hart <james.s.hart@outlook.com>
2019-09-01 15:27:21 +0100

    added "Line" component

---
+++ src/decor/Line.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+
+import ThemeContext from '../ThemeContext';
+
+const Button: React.FunctionComponent<{ vertical?: boolean, horizontal?: boolean }> = (props) => {
+	return (
+		<ThemeContext.Consumer>
+			{(theme) => {
+				if (props.horizontal) {
+					return (
+						<div style={{ background: theme.baseMediumLow, height: 2, margin: "7px 32px" }} />
+					);
+				} else if (props.vertical) {
+					return (
+						<div style={{ background: theme.baseMediumLow, width: 2, margin: "8px 7px" }} />
+					);
+				} else {
+					throw "Line (Component): Please specify direction!";
+				}
+			}}
+		</ThemeContext.Consumer>
+	)
+}
+
+export default Button;
