c7dd007ca90662e6e2a18771d0f943b69cd59cea
James Hart <james.s.hart@outlook.com>
2019-09-04 17:16:40 +0100

    consistent styling

--- src/navigation/topnav/navbutton.tsx
+++ src/navigation/topnav/navbutton.tsx
@@ -4,24 +4,6 @@ import { styles as typeStyles } from '../../typography/styles';
 
 import styles from './rwf.css';
 
-import { Depths } from '@uifabric/fluent-theme';
-
-const showHoverStyles: (e: React.MouseEvent<HTMLDivElement>) => void = (e) => {
-	e.currentTarget.style.boxShadow = Depths.depth16;
-}
-
-const hideHoverStyles: (e: React.MouseEvent<HTMLDivElement>) => void = (e) => {
-	e.currentTarget.style.boxShadow = "";
-}
-
-const showActiveStyles: (e: React.MouseEvent<HTMLDivElement>) => void = (e) => {
-	e.currentTarget.style.boxShadow = "inset 0 0 2px 1px #000000";
-}
-
-const hideActiveStyles: (e: React.MouseEvent<HTMLDivElement>) => void = (e) => {
-	e.currentTarget.style.boxShadow = "";
-}
-
 export default class NavButton extends React.Component<{onClick?: React.MouseEventHandler<HTMLDivElement>, id?: string, right?: boolean}> {
 	render() {
 		const {
@@ -34,12 +16,8 @@ export default class NavButton extends React.Component<{onClick?: React.MouseEve
 		const floatRight = right ? { marginLeft: "auto" } : {};
 
 		return (
-			<div onClick={onClick}
-				onMouseOver={showHoverStyles}
-				onMouseLeave={hideHoverStyles}
-				onMouseDown={showActiveStyles}
-				onMouseUp={hideActiveStyles}
-				className={styles.navitem} id={id}
+			<div onClick={onClick} id={id}
+				className={styles.navitem}
 				style={{ ...floatRight}}>
 				<span style={{ ...typeStyles.Base }}>{children}</span>
 			</div>
