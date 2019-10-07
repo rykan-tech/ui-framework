8466fc2ad46afb589c835216f174ed96468acfcd
James Hart <james.s.hart@outlook.com>
2019-10-07 17:50:25 +0100

    made class name work better

--- src/navigation/sidebar/index.tsx
+++ src/navigation/sidebar/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 import styles from './r_sidebar.css';
 
-export default class Sidebar extends React.Component<{compact?: boolean, style?: React.CSSProperties, children?: any}> {
+export default class Sidebar extends React.Component<{compact?: boolean, style?: React.CSSProperties, children?: any, className?: string}> {
 	render() {
 		const {
 			children,
@@ -15,7 +15,7 @@ export default class Sidebar extends React.Component<{compact?: boolean, style?:
 		if (compact) width = 240;
 
 		return (
-			<div className={styles.sidebar} style={{ ...style, width: width }}>
+			<div className={this.props.className ? styles.sidebar + " " + this.props.className : styles.sidebar} style={{ ...style, width: width }}>
 				{children}
 			</div>
 		)
