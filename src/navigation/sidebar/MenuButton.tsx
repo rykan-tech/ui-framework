18937d2cbea7712e38e48520789739fb66a5500c
James Hart <james.s.hart@outlook.com>
2019-10-07 17:50:42 +0100

    add style prop

--- src/navigation/sidebar/MenuButton.tsx
+++ src/navigation/sidebar/MenuButton.tsx
@@ -4,9 +4,9 @@ import styles from './r_sidebar.css';
 
 import { Icon } from 'office-ui-fabric-react';
 
-const MenuButton: React.FunctionComponent<{ icon?: string, children: string, onClick?: React.MouseEventHandler }> = (props) => {
+const MenuButton: React.FunctionComponent<{ icon?: string, children: string, onClick?: React.MouseEventHandler, style?: React.CSSProperties }> = (props) => {
 	return (
-		<button onClick={props.onClick} className={styles.menu_button}>
+		<button onClick={props.onClick} className={styles.menu_button} style={props.style}>
 			<Icon className={styles.menu_button_icon} iconName={props.icon} />
 			<label className={styles.menu_button_label}>{props.children}</label>
 		</button>
