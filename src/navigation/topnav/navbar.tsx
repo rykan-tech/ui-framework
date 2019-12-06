bedac006269b7ce206495c191a55bb5a6f08e8ea
James Hart <james.s.hart@outlook.com>
2019-12-06 14:43:30 +0000

    navbar tweaks

--- src/navigation/topnav/navbar.tsx
+++ src/navigation/topnav/navbar.tsx
@@ -4,13 +4,14 @@ import styles from './rwf.css';
 
 import ThemeContext from '../../ThemeContext';
 
-export default class Navbar extends React.Component<{logo?: string, style?: React.CSSProperties, outerStyle?: React.CSSProperties, fixed?: boolean}> {
+export default class Navbar extends React.Component<{ logo?: string, style?: React.CSSProperties, outerStyle?: React.CSSProperties, fixed?: boolean, onClickToggle?: React.MouseEventHandler }> {
 	render() {
 		const {
 			children,
 			logo,
 			style,
 			outerStyle,
+			onClickToggle,
 		} = this.props;
 
 		return (
@@ -19,6 +20,9 @@ export default class Navbar extends React.Component<{logo?: string, style?: Reac
 					return (
 						<div className={styles.navbar} style={outerStyle}>
 							<div className={styles["navbar-inner"]} style={{ background: theme.altMediumHigh, ...style }}>
+								<div className={styles.toggle} onClick={onClickToggle}>
+									<span style={{ display: "inline-flex", margin: "auto", fontFamily: "monospace" }}>&#9776;</span>
+								</div>
 								<img className={styles.rykan} alt="Logo" src={logo} />
 								{children}
 							</div>
