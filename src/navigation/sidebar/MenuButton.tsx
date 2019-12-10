449dd77c807de5e3e9db61c96b79d07d9b23d840
James Hart <james.s.hart@outlook.com>
2019-12-10 21:31:07 +0000

    add data-url to menu button

--- src/navigation/sidebar/MenuButton.tsx
+++ src/navigation/sidebar/MenuButton.tsx
@@ -4,9 +4,9 @@ import styles from './r_sidebar.css';
 
 import { Icon } from 'office-ui-fabric-react';
 
-const MenuButton: React.FunctionComponent<{ icon?: string, children: string, onClick?: React.MouseEventHandler, style?: React.CSSProperties }> = (props) => {
+const MenuButton: React.FunctionComponent<{ icon?: string, data?: { url: string } | undefined, children: string, onClick?: React.MouseEventHandler, style?: React.CSSProperties }> = (props) => {
 	return (
-		<button onClick={props.onClick} className={styles.menu_button} style={props.style}>
+		<button onClick={props.onClick} className={styles.menu_button} style={props.style} data-url={props.data ? props.data.url : null} {...props.data}>
 			<Icon className={styles.menu_button_icon} iconName={props.icon} />
 			<label className={styles.menu_button_label}>{props.children}</label>
 		</button>
